6b562444d3092fdb406dc7a70c89e8aa10e3c6c3
Sindre <sndrem@live.no>
2015-01-06 14:13:48 +0100

    Må fikse form-validation

--- formvalidation.js
+++ formvalidation.js
@@ -15,24 +15,31 @@ function validateForm(){
 	
 	if(form.fullName.value === ""){
 		$("#fullName").attr('placeholder', 'Vennligst fyll inn fullt navn');
-	};
+	}
 
 	if(form.adress.value === ""){
 		$("#adress").attr('placeholder', "Vennligst fyll inn full adresse");
-	};
+	}
 
 	if(form.phone.value.length !== 8){
 		$("#phone").attr('placeholder', "Vennligst fyll inn et gyldig telefonnummer med 8 siffer");
-	};
+	}
 
 	if(!validateEmail(form.email.value) || form.email.value == ""){
 		$("#email").attr('placeholder', "Vennligst bruk en gyldig email-adresse");
-	};
+	}
 
-	var width = parseInt(form.width.value);
-	if(width < 10){
+	
+	if(parseInt(form.width.value) || form.width.value === ""){
 		$("#width").attr('placeholder', 'Vennligst tast inn ønsket bredde på bordet.');
-	};
+	}
+
+	if(parseInt(form.length.value) < 0 || form.length.value === ""){
+		$("#length").attr('placeholder', 'Vennligst tast inn ønsket lengde på bordet.');
+	}
+	if(calculateSize(parseInt(form.width.value), parseInt(form.length.value)) > 0){
+	$("#beregnetStorrelse").html("Beregnet størrelse: " + calculateSize(parseInt(form.width.value), parseInt(form.length.value)) + " kvadratmeter.");
+	}
 };
 
 function resetForm(){
@@ -43,3 +50,9 @@ function validateEmail(email) {
     var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email);
 }; 
+
+function calculateSize(width, length){
+	var size = width * length;
+
+	return size;
+}
