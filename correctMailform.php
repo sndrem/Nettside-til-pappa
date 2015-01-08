7ffcb0d5b4abebf5d378a528ff245b0351dff3d8
Sindre <sndrem@live.no>
2015-01-08 19:48:54 +0100

    Nada

--- correctMailform.php
+++ correctMailform.php
@@ -6,21 +6,26 @@ $mail = new PHPmailer;
 
 $mail->isSMTP();
 $mail->SMTPAuth = true;
-$mail->SMTPDebug = 3;
+$mail->SMTPDebug = 4;
+$mail->Mailer = 'smtp';
+$mail->SMTPSecure = 'ssl';
 $mail->Host = 'smtp.gmail.com';
+$mail->setFrom('sndrem@gmail.com', 'Sindre Moldeklev');
+$mail->Port = 465;
 $mail->Username = 'sndrem@gmail.com';
 $mail->Password = 'handball:11';
-$mail->SMTPSecure = 'ssl';
-$mail->Port = 465;
+
 
 $mail->From = 'sndrem@gmail.com';
 $mail->FromName = 'Sindre Moldeklev';
-$mail->addReplyTo('sndrem@gmail.com', 'Reply adress');
-$mail->addAddress('sndrem@gmail.com', 'Sindre Moldeklev');
+$mail->addReplyTo('sim@tv2.no', 'Reply adress');
+$mail->addAddress('sim@tv2.no', 'Sindre Moldeklev');
 
 $mail->Subject = 'Here is an email';
 $mail->Body = 'This is the body of the email';
 $mail->AltBody = 'This is the alternative body';
+
+echo (extension_loaded('openssl')?'SSL loaded':'SSL not loaded')."\n"; 
 
 var_dump($mail->send());
 
